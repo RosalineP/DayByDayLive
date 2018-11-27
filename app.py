327aaafa1bc2797a176869fbcf2abd598f04580a
RosalineP <rpyktel@gmail.com>
2018-11-27 10:37:05 -0800

    File linking for setup

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template, flash, request, redirect, url_for, session, jsonify
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask, dude!'
+def initialPage():
+    return render_template('base.html')
